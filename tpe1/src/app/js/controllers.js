938697f81f2f8524e36b4cd427bafbe1fbaeba9b
Kevin J. Hanna <kevinjackhanna@gmail.com>
2013-09-26 18:20:32 -0300

    Show grid view for search and categories

--- tpe1/src/app/js/controllers.js
+++ tpe1/src/app/js/controllers.js
@@ -6,6 +6,25 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
+  .controller('ProductsCtrl', ['$scope', function(sc) {
+    sc.products = [
+      { title: "Zapato Rojo", price: 21.50 },
+      { title: "Zapato Rojo", price: 21.50 },
+      { title: "Zapato Rojo", price: 21.50 },
+      { title: "Zapato Rojo", price: 21.50 },
+      { title: "Zapato Rojo", price: 21.50 },
+      { title: "Zapato Azul", price: 24.50 }
+    ];
+
+    sc.categories = [
+      { title: "Pantalones", active: false },
+      { title: "Remeras", active: true },
+      { title: "Zapatos", active: false },
+      { title: "Anteojos", active: false }
+    ];
+
+
+  }])
   .controller('MyCtrl2', [function() {
 
   }]);
