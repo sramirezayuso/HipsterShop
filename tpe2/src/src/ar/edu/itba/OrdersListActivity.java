9cd6a3788ac44c2b161664761ffd61e2d111ec25
Kevin J. Hanna <kevinjackhanna@gmail.com>
2013-11-16 19:45:26 -0300

    Fetch Orders and show them on a ListView
    (Only their status for now)

--- tpe2/src/src/ar/edu/itba/OrdersListActivity.java
+++ tpe2/src/src/ar/edu/itba/OrdersListActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import ar.edu.itba.model.GetAllOrders;
+import ar.edu.itba.model.Order;
 import ar.edu.itba.services.ApiService;
 import ar.edu.itba.utils.HipsterShopApi;
 import ar.edu.itba.utils.Utils;
@@ -17,18 +19,9 @@ public class OrdersListActivity extends MasterActivity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_orders_list);
-		
 
         final Intent intent = HipsterShopApi.getAllOrdersRequest(this, apiResultReceiver);
         startService(intent);
-		
-		String[] myStringArray = new String[]{"hola", "chau"};
-		ArrayAdapter adapter = new ArrayAdapter<String>(this, 
-		        android.R.layout.simple_list_item_1, myStringArray);
-		ListView listView = (ListView) findViewById(R.id.listView1);
-		listView.setAdapter(adapter);
-	    // TODO Auto-generated method stub
-
 	}
 	
 
@@ -42,15 +35,14 @@ public class OrdersListActivity extends MasterActivity {
 
             break;
         case ApiService.STATUS_FINISHED:
-        	System.out.println("Lo que vuelve de response a la Activity: "+ resultData.get("response"));
-        	GetAllOrders orders = (GetAllOrders) resultData.get(Utils.RESPONSE);
-        	System.out.println("los states son: "+ orders.getOrders());
-        	
-        	System.out.println("El primero es: "+ orders.getOrders().get(0).getId()+ " " + orders.getOrders().get(0).getName());
-
-        	
-        	//System.out.println("Lo que vuelve de response a la Activity: " + results);
-            // hide progress
+        	GetAllOrders response = (GetAllOrders) resultData.get(Utils.RESPONSE); 
+        	System.out.println(response.getOrders());
+        	Order[] orders = response.getOrders().toArray(new Order[response.getOrders().size()]); // Just to have sth to show for now
+    		    		 
+    		ArrayAdapter<Order> adapter = new ArrayAdapter<Order>(this, 
+    		        android.R.layout.simple_list_item_1, orders);
+    		ListView listView = (ListView) findViewById(R.id.listView1);
+    		listView.setAdapter(adapter);
             break;
         case ApiService.STATUS_ERROR:
         	System.out.println("error");
