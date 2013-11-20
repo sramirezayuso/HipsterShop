b5fd0442925c30f2a9f5a65133ef0b088ed4c263
iojichervo <ioji@chervonagura.com.ar>
2013-11-20 02:23:29 -0300

    Add products (grid) activity

---
+++ tpe2/src/src/ar/edu/itba/ProductsActivity.java
@@ -0,0 +1,86 @@
+package ar.edu.itba;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import ar.edu.itba.model.Attribute;
+import ar.edu.itba.model.Product;
+import ar.edu.itba.utils.ProductAdapter;
+
+public class ProductsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_products);
+		// Show the Up button in the action bar.
+		setupActionBar();
+
+		GridView gridView = (GridView) findViewById(R.id.gridview);
+
+		ArrayList<Product> data = new ArrayList<Product>();
+		String[] b = { "Levi's" };
+		Attribute[] atts = new Attribute[1];
+		atts[0] = new Attribute(9, "marca", b);
+		data.add(new Product("Baker4", new Integer(100), atts));
+		data.add(new Product("Baker5", new Integer(3400), atts));
+		data.add(new Product("Baker3", new Integer(60), atts));
+
+		ProductAdapter imageAdapter = new ProductAdapter(this, data);
+		gridView.setAdapter(imageAdapter);
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				Intent intent = new Intent(ProductsActivity.this,
+						ProdActivity.class);
+				ProductsActivity.this.startActivity(intent);
+
+			}
+		});
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.products, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ tpe2/src/src/ar/edu/itba/utils/ProductAdapter.java
@@ -0,0 +1,74 @@
+package ar.edu.itba.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import ar.edu.itba.R;
+import ar.edu.itba.model.Attribute;
+import ar.edu.itba.model.Product;
+
+public class ProductAdapter extends BaseAdapter {
+	private List<Product> items = new ArrayList<Product>();
+	private LayoutInflater inflater;
+
+	public ProductAdapter(Context context, List<Product> items) {
+		inflater = LayoutInflater.from(context);
+		this.items = items;
+	}
+
+	@Override
+	public int getCount() {
+		return items.size();
+	}
+
+	@Override
+	public Object getItem(int i) {
+		return items.get(i);
+	}
+
+	@Override
+	public long getItemId(int i) {
+		return Long.parseLong(items.get(i).getName());
+	}
+
+	@Override
+	public View getView(int i, View view, ViewGroup viewGroup) {
+		View v = view;
+		ImageView picture;
+		TextView data;
+
+		if (v == null) {
+			v = inflater.inflate(R.layout.grid_element, viewGroup, false);
+			v.setTag(R.id.itemImage, v.findViewById(R.id.itemImage));
+			v.setTag(R.id.itemData, v.findViewById(R.id.itemData));
+		}
+
+		picture = (ImageView) v.getTag(R.id.itemImage);
+		data = (TextView) v.getTag(R.id.itemData);
+
+		Product item = (Product) getItem(i);
+
+		new DownloadImageTask(picture).execute(item.getImageUrl()[0]);
+		data.setText(getProductData(item));
+
+		return v;
+	}
+
+	private String getProductData(Product prod) {
+		String brand = "";
+		for (Attribute att : prod.getAttributes()) {
+			if (att.getId() == 9 && att.getName().equals("marca")) {
+				brand = att.getValues()[0];
+			}
+		}
+		return prod.getName() + '\n' + "$" + prod.getPrice() + " - " + brand;
+	}
+
+}
